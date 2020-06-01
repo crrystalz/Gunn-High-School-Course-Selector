b2ca534c28c16964825419d73c2b12b9766f9b2a
Rrishi Anand <lichupatnaik@gmail.com>
2020-05-31 21:27:25 -0700

    Added more basic D3.js code
    Note: These first couple commits are just me learning about the D3.js web framework. The code I am writing will not be used in the actual project.

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ d3.select('body').append('p').text('Third Paragraph');
 d3.selectAll('p').style('color', 'blue');
 */
 
+/*
 var dataset = [1, 2, 3, 4, 5];
 
 d3.select('body')
@@ -22,3 +23,32 @@ d3.select('body')
     .append('p')
     //.text('D3 is awesome!!');
     .text(function(d) { return d; });
+*/
+
+var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+
+var svgWidth = 500,
+    svgHeight = 300,
+    barPadding = 5;
+var varWidth = (svgWidth / dataset.length);
+
+
+var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return svgHeigh - dl
+    })
+    .attr("height", function(d) {
+        return (d);
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+        var translate = [varWidth * i, 0];
+        return "translate(" + translate + ")";
+    });
